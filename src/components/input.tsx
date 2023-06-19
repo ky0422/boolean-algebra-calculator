a99243d3a035e7cbc37a34c45321848d32069ba6
ky0422 <ky0422a@gmail.com>
2023-06-19 18:59:48 +0900

    `update`: updated Component

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,6 +1,8 @@
 interface InputProps {
     value: string
     submit: (value: string) => void
+    textClassNames?: string
+    submitClassNames?: string
 }
 
 export default (props: InputProps) => (
@@ -10,7 +12,7 @@ export default (props: InputProps) => (
             props.submit((e.target as any)[0].value)
         }}
     >
-        <input type='text' defaultValue={props.value} />
-        <input type='submit' value='Calculate' />
+        <input type='text' defaultValue={props.value} className={props.textClassNames} />
+        <input type='submit' value='Calculate' className={props.submitClassNames} />
     </form>
 )
